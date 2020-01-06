7f7c96823a74f18689c27542b3467155bb9d34b1
lpalazzi <lpalazzi@outlook.com>
2020-01-06 18:30:55 -0500

    update test suite

---
+++ testSuite/NOT-INCL-YET-injections-lenet-mnist.py
@@ -0,0 +1,155 @@
+#!/usr/bin/python
+'''
+LeNet CNN for MNIST
+Project: https://github.com/sujaybabruwad/LeNet-in-Tensorflow
+'''
+
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
+import random
+from sklearn.utils import shuffle
+import tensorflow as tf
+import TensorFI as ti
+import os
+
+mnist = input_data.read_data_sets("MNIST_data/", reshape=False)
+X_train, y_train           = mnist.train.images, mnist.train.labels
+X_validation, y_validation = mnist.validation.images, mnist.validation.labels
+X_test, y_test             = mnist.test.images[:256], mnist.test.labels[:256]
+
+assert(len(X_train) == len(y_train))
+assert(len(X_validation) == len(y_validation))
+assert(len(X_test) == len(y_test))
+
+print("Image Shape: {}".format(X_train[0].shape))
+print("Training Set:   {} samples".format(len(X_train)))
+print("Validation Set: {} samples".format(len(X_validation)))
+print("Test Set:       {} samples".format(len(X_test)))
+
+# Pad images with 0s
+X_train      = np.pad(X_train, ((0,0),(2,2),(2,2),(0,0)), 'constant')
+X_validation = np.pad(X_validation, ((0,0),(2,2),(2,2),(0,0)), 'constant')
+X_test       = np.pad(X_test, ((0,0),(2,2),(2,2),(0,0)), 'constant')
+    
+print("Updated Image Shape: {}".format(X_train[0].shape))
+
+index = random.randint(0, len(X_train))
+image = X_train[index].squeeze()
+
+print(y_train[index])
+
+X_train, y_train = shuffle(X_train, y_train)
+
+EPOCHS = 20
+BATCH_SIZE = 128
+
+def LeNet(x):    
+    # Hyperparameters
+    mu = 0
+    sigma = 0.1
+    layer_depth = {
+        'layer_1' : 6,
+        'layer_2' : 16,
+        'layer_3' : 120,
+        'layer_f1' : 84
+    }
+
+	# Layer 1: Convolutional. Input = 32x32x1. Output = 28x28x6.
+    conv1_w = tf.Variable(tf.truncated_normal(shape = [5,5,1,6],mean = mu, stddev = sigma))
+    conv1_b = tf.Variable(tf.zeros(6))
+    conv1 = tf.nn.conv2d(x,conv1_w, strides = [1,1,1,1], padding = 'VALID') + conv1_b 
+    # Activation.
+    conv1 = tf.nn.relu(conv1)
+
+    # Pooling. Input = 28x28x6. Output = 14x14x6.
+    pool_1 = tf.nn.max_pool(conv1,ksize = [1,2,2,1], strides = [1,2,2,1], padding = 'VALID')
+    
+    # Layer 2: Convolutional. Output = 10x10x16.
+    conv2_w = tf.Variable(tf.truncated_normal(shape = [5,5,6,16], mean = mu, stddev = sigma))
+    conv2_b = tf.Variable(tf.zeros(16))
+    conv2 = tf.nn.conv2d(pool_1, conv2_w, strides = [1,1,1,1], padding = 'VALID') + conv2_b
+    # Activation.
+    conv2 = tf.nn.relu(conv2)
+
+    # Pooling. Input = 10x10x16. Output = 5x5x16.
+    pool_2 = tf.nn.max_pool(conv2, ksize = [1,2,2,1], strides = [1,2,2,1], padding = 'VALID') 
+
+    pool_2_shape = pool_2.get_shape().as_list()
+    # Flatten. Input = 5x5x16. Output = 400.
+    fc1 = tf.reshape(pool_2, [-1, pool_2_shape[1]*pool_2_shape[2]*pool_2_shape[3]])
+    # fc1 = tf.contrib.layers.flatten(pool_2)
+
+    # Layer 3: Fully Connected. Input = 400. Output = 120.
+    fc1_w = tf.Variable(tf.truncated_normal(shape = (400,120), mean = mu, stddev = sigma))
+    fc1_b = tf.Variable(tf.zeros(120))
+    fc1 = tf.matmul(fc1,fc1_w) + fc1_b
+    
+    # Activation.
+    fc1 = tf.nn.relu(fc1)
+
+    # Layer 4: Fully Connected. Input = 120. Output = 84.
+    fc2_w = tf.Variable(tf.truncated_normal(shape = (120,84), mean = mu, stddev = sigma))
+    fc2_b = tf.Variable(tf.zeros(84))
+    fc2 = tf.matmul(fc1,fc2_w) + fc2_b
+    # Activation.
+    fc2 = tf.nn.relu(fc2)
+
+    # Layer 5: Fully Connected. Input = 84. Output = 10.
+    fc3_w = tf.Variable(tf.truncated_normal(shape = (84,10), mean = mu , stddev = sigma))
+    fc3_b = tf.Variable(tf.zeros(10))
+    logits = tf.matmul(fc2, fc3_w) + fc3_b
+    return logits
+
+x = tf.placeholder(tf.float32, (None, 32, 32, 1))
+y = tf.placeholder(tf.int32, (None))
+one_hot_y = tf.one_hot(y, 10)
+
+rate = 0.001
+
+logits = LeNet(x)
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits = logits, labels = one_hot_y)
+loss_operation = tf.reduce_mean(cross_entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate = rate)
+training_operation = optimizer.minimize(loss_operation)
+
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
+accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+saver = tf.train.Saver()
+
+def evaluate(X_data, y_data):
+    num_examples = len(X_data)
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range(0, num_examples, BATCH_SIZE):
+        batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    num_examples = len(X_train)
+    print("Training...")
+    print()
+    for i in range(EPOCHS):
+        X_train, y_train = shuffle(X_train, y_train)
+        for offset in range(0, num_examples, BATCH_SIZE):
+            end = offset + BATCH_SIZE
+            batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
+        validation_accuracy = evaluate(X_validation, y_validation)
+        print("EPOCH {} ...".format(i+1))
+        print("Validation Accuracy = {:.3f}".format(validation_accuracy))
+    saver.save(sess, './lenet')
+    print("Model saved")
+
+with tf.Session() as sess:
+    saver.restore(sess, tf.train.latest_checkpoint('.'))
+
+    test_accuracy = evaluate(X_test, y_test)
+    print("Accuracy (with no injections): {:.3f}".format(test_accuracy))
+
+    # Add the fault injection code here to instrument the graph
+    fi = ti.TensorFI(sess, logLevel = 10, name = "lenet", disableInjections=False)
+    test_accuracy = evaluate(X_test, y_test)
+    print("Accuracy (with injections): {:.3f}".format(test_accuracy))
---
+++ testSuite/REFERENCE_empty_test_script.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+# This is an empty test script that follows the format for proper inclusion in the runAll.py main script
+# To include a new test in the suite, add the following lines of code to runAll.py
+#      
+#  import REFERENCE_empty_test_script
+#  sys.stdout.write("Running REFERENCE_empty_test_script test...")
+#  sys.stdout.flush()
+#  if REFERENCE_empty_test_script.run_test(suppress_out=True):
+#      sys.stdout.write("\rRunning REFERENCE_empty_test_script test... Passed\n")
+#      sys.stdout.flush()
+#  else:
+#      failed_tests.append("REFERENCE_empty_test_script.py")
+#      sys.stdout.write("\rRunning REFERENCE_empty_test_script test... Failed\n")
+#      sys.stdout.flush()
+#
+# The main code for the test should go inside the run_test() function, as indicated
+
+# set logging folder
+from globals import TESTSUITE_DIR
+logDir   = TESTSUITE_DIR + "/faultLogs/"
+confFile = TESTSUITE_DIR + "/confFiles/***_config.yaml" #specify the config file
+# ***the call to TensorFI to instrument the model must include these directories, e.g.,  fi = ti.TensorFI(..., configFileName= confFile, logDir=logDir)
+
+# suppress warnings
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import yaml
+yaml.warnings({'YAMLLoadWarning': False})
+
+import tensorflow as tf
+import TensorFI as ti
+import sys
+
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+def run_test(suppress_out=False):
+
+    if suppress_out:
+        sys.stdout = open(os.devnull, "w")
+        sys.stderr = open(os.devnull, "w")
+
+    # ************************
+    # ************************
+    # MAIN TEST CODE GOES HERE
+    # ************************
+    # ************************
+
+    if suppress_out:
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
+    
+    return passed_bool # set this based on the test requirements (True if the test passed, False otherwise)
+
+if __name__ == "__main__":
+    passed_bool = run_test()
+
+    print "\n\nTEST RESULTS"
+    if passed_bool:
+        print "Test passed"
+    else:
+        print "Test failed"
