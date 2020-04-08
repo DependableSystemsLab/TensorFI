68d7c37e474f9fb1dcd8f2af6537c9e6366d2998
Niranjhana Narayanan <nniranjhana@ece.ubc.ca>
2020-04-07 23:27:13 -0700

    Update setup.py for pip release of v2.0
    Signed-off-by: Niranjhana Narayanan <nniranjhana@ece.ubc.ca>

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ from distutils.core import setup
 setup(
   name = 'TensorFI',
   packages = ['TensorFI'],
-  version = '1.0.0',
+  version = '2.0.0',
   license='MIT',
   description = 'A fault injection tool for TensorFlow-based program',
   author = 'DependableSystemsLab',
   author_email = 'karthikp@ece.ubc.ca',
   url = 'https://github.com/DependableSystemsLab/TensorFI',
-  download_url = 'https://github.com/DependableSystemsLab/TensorFI/archive/v1.0.0.tar.gz',
+  download_url = 'https://github.com/DependableSystemsLab/TensorFI/archive/v2.0.0.tar.gz',
   long_description= "TensorFI is a fault injection framework for injecting both hardware and software faults into applications written using the TensorFlow framework.   \
                       Check the GitHub repo for more details",
   install_requires=[
